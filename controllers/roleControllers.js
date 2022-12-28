2bfe405e350d83ca0416cda421a061b5476e28bc
amrit578singh@gmail.com <amritsingh@innotechteam.in>
2022-12-28 13:28:33 +0530

    getRole

--- controllers/roleControllers.js
+++ controllers/roleControllers.js
@@ -10,6 +10,18 @@ const addRole = async (req, res) => {
     res.status(500).send({ message: "500 error to user" });
   }
 };
+
+const getAllRole = async (req, res) => {
+  try {
+    const findData = await role.findAll();
+    res.status(200).send(findData);
+  } catch (error) {
+    res.status(500).send({
+      message: "500 error to user",
+    });
+  }
+};
 module.exports = {
   addRole,
+  getAllRole,
 };
--- index.js
+++ index.js
@@ -9,13 +9,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-const { addRole } = require("./controllers/roleControllers");
+const { addRole, getAllRole } = require("./controllers/roleControllers");
 
 const role = require("./models/role");
 
 role.sync({ alter: true });
 
 app.post("/role/add", addRole);
+app.get("/role/list", getAllRole);
 
 const port = process.env.port || 8000;
 app.listen(port);
