8c636cad768528a540487cbab8aebda567f24e40
amrit578singh@gmail.com <amritsingh@innotechteam.in>
2022-12-30 17:09:41 +0530

    Changes done

--- controllers/userController.js
+++ controllers/userController.js
@@ -36,21 +36,19 @@ const userRegister = async (req, res) => {
 const login = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
     const userData = await user.findOne({
       where: {
         email,
       },
     });
     if (!userData) {
-      res.status(401).send({ message: "Please provide correct details" });
+      res.status(401).send({ message: "500 Error to user" });
     } else {
       const isMatch = await bcrypt.compare(password, userData.password);
       if (isMatch) {
         res.status(200).send({ id: userData.id });
       } else {
-        res.status(500).send({ message: "Password is incorrect" });
+        res.status(500).send({ message: "500 error to user" });
       }
     }
   } catch (error) {
