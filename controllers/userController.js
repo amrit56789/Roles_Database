8b216d81b4e23a3dc9d921f50e8400885244261c
amrit578singh@gmail.com <amritsingh@innotechteam.in>
2022-12-30 13:05:28 +0530

    Changes complete

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const bcrypt = require("bcrypt");
+
 const sequelize = require("../util/database");
 const user = require("../models/user");
 
--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const Sequelize = require("sequelize");
 
 const sequelize = require("../util/database");
 const Role = require("../models/role");
+
 const user = sequelize.define(
   "user",
   {
